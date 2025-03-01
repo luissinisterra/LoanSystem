bbe4b549e461e60fd58f4849a463e759cb103cbc
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2025-03-01 17:04:54 -0500

    Commit inicial

---
+++ src/app/Application.java
@@ -0,0 +1,37 @@
+package app;
+
+import app.manager.FormsManager;
+import app.views.Login;
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import raven.toast.Notifications;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Application extends JFrame {
+
+    public Application() {
+        init();
+    }
+
+    private void init() {
+        setTitle("FlatLaf Login");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(new Dimension(1200, 700));
+        setLocationRelativeTo(null);
+        setContentPane(new Login());
+        Notifications.getInstance().setJFrame(this);
+        FormsManager.getInstance().initApplication(this);
+    }
+
+    public static void main(String[] args) {
+        FlatRobotoFont.install();
+        FlatLaf.registerCustomDefaultsSource("raven.themes");
+        UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 13));
+        FlatMacDarkLaf.setup();
+        EventQueue.invokeLater(() -> new Application().setVisible(true));
+    }
+}
+
