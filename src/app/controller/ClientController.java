d550217b71aa36adf5cd8112932ce16a9d61a57b
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2025-04-05 22:59:57 -0500

    Implementado la funcionalidad de guardar en la gestion de clientes

--- src/app/controller/ClientController.java
+++ src/app/controller/ClientController.java
@@ -2,6 +2,7 @@ package app.controller;
 
 import app.model.Client;
 import app.service.ClientService;
+import retrofit2.http.Path;
 
 import java.util.List;
 
@@ -15,4 +16,21 @@ public class ClientController {
     public List<Client> getAllClients() {
         return this.clientService.getAllClients();
     }
+
+    public void createClient(Client client) {
+        this.clientService.createClient(client);
+    }
+
+    public void updateClient(String id, Client client) {
+        this.clientService.updateClient(id, client);
+    }
+
+    public void deleteClient(String id) {
+        this.clientService.deleteClient(id);
+    }
+
+    public List<Client> searchClientsByQuery(String query) {
+        return this.clientService.searchClientsByQuery(query);
+    }
+
 }
--- src/app/service/imp/IClientService.java
+++ src/app/service/imp/IClientService.java
@@ -2,11 +2,26 @@ package app.service.imp;
 
 import app.model.Client;
 import retrofit2.Call;
-import retrofit2.http.GET;
+import retrofit2.http.*;
 
 import java.util.List;
 
 public interface IClientService {
     @GET("/clients")
     Call<List<Client>> getAllClients();
+
+    @GET("/clients/{id}")
+    Call<Client> getClientById(@Path("id") String id);
+
+    @POST("/clients")
+    Call<Void> createClient(@Body Client client);
+
+    @PUT("/clients/{id}")
+    Call<Void> updateClient(@Path("id") String id, @Body Client client);
+
+    @DELETE("/clients/{id}")
+    Call<Void> deleteClient(@Path("id") String id);
+
+    @GET("/clients/search")
+    Call<List<Client>> searchClientsByQuery(@Query("query") String query);
 }
